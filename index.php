56da84ee37ae6cfc19bcac4ada901cc3752ab05e
Marjana Begum <marjanamarjanabegum5@gmail.com>
2024-07-13 23:00:35 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans+Caption:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+
+<body>
+
+    <section class="hero">
+        <header id="header">
+            <a id="logo" href="#">Marjana</a>
+        </header>
+        <header class="hero-header">
+            <h1 class="hero-title">Php Login and Sign Up</h1>
+        </header>
+        <footer class="hero-footer">
+            <a class="button button-primary" href="login.php">Login</a>
+            <a class="button" href="signup.php">Signup</a>
+        </footer>
+    </section>
+
+
+</body>
+
+</html>
