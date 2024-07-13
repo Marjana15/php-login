65331003500be02fdbd9d9adf764189f871b9415
Marjana Begum <marjanamarjanabegum5@gmail.com>
2024-07-13 23:00:42 +0600

    Create loginsignup.sql

---
+++ loginsignup.sql
@@ -0,0 +1,20 @@
+--
+-- Database: `loginphp`
+--
+CREATE DATABASE IF NOT EXISTS `loginphp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `loginphp`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
